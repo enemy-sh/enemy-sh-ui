f02c69f1566a8cb6657730e0d50c99602a6f6b58
Eric <eric@vedbyte.dev>
2024-11-20 12:26:21 -0500

    fixed reactive content on services carousel

--- components/ui/services-carousel.tsx
+++ components/ui/services-carousel.tsx
@@ -7,53 +7,71 @@ import "slick-carousel/slick/slick-theme.css";
 import { Button } from "@/components/ui/button";
 
 interface ServicesCarouselProps {
-    services: {
-        title: string;
-        description: string;
-        categories: string[];
-    }[];
+  services: {
+    title: string;
+    description: string;
+    categories: string[];
+  }[];
 }
 
 const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    cssEase: 'ease',
-    pauseOnHover: true,
-    arrows: true,
-    centerMode: false,
-    variableWidth: false,
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  cssEase: 'ease',
+  pauseOnHover: true,
+  arrows: true,
+  centerMode: false,
+  variableWidth: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true
+      }
+    }
+  ]
 }
 
 export default function ServicesCarousel({ services }: ServicesCarouselProps) {
-    return (
-      <div className="w-full h-fit p-2 justify-center flex flex-col">
-        <Slider {...settings}>
-            {services.map((service, index) => (
-              <div key={index} className="p-2">
-                <div key={service.title} className="flex flex-col items-start h-fit text-gray-800 justify-start sm:w-auto gap-4 p-4 bg-gray-100">
-                  <div className="flex flex-col items-start justify-between w-full">
-                    <div className="flex flex-row items-start justify-between w-full">
-                      <p className="text-lg font-semibold">{service.title}</p>
-                    </div>
-                    <p className="text-gray-700 text-sm">{service.description}</p>
-                  </div>
-                  <div className="flex flex-row items-center justify-start w-full gap-2">
-                  <Button variant={'ghost'}>learn more</Button>
-                  </div>
-                  <div className="flex flex-row items-center justify-start w-full gap-2">
-                    {service.categories.map((category, index) => (
-                      <div key={index} className="bg-black transition-all  text-slate-200 px-4 py-1 text-xs">{category}</div>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            ))}
-        </Slider>
-      </div>
-    )
+  return (
+    <div className="w-full h-fit p-2 justify-center flex flex-col">
+    <Slider {...settings}>
+      {services.map((service, index) => (
+        <div key={index} className="p-2">
+        <div key={service.title} className="flex flex-col items-start h-fit text-gray-800 justify-start sm:w-auto gap-4 p-4 bg-gray-100">
+          <div className="flex flex-col items-start justify-between w-full">
+          <div className="flex flex-row items-start justify-between w-full">
+            <p className="text-lg font-semibold">{service.title}</p>
+          </div>
+          <p className="text-gray-700 text-sm">{service.description}</p>
+          </div>
+          <div className="flex flex-row items-center justify-start w-full gap-2">
+          <Button variant={'ghost'}>learn more</Button>
+          </div>
+          <div className="flex flex-row items-center justify-start w-full gap-2 flex-wrap">
+          {service.categories.map((category, index) => (
+            <div key={index} className="bg-black transition-all text-slate-200 px-4 py-1 text-xs">{category}</div>
+          ))}
+          </div>
+        </div>
+        </div>
+      ))}
+    </Slider>
+    </div>
+  )
 }
