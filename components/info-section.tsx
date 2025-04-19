a3cd7835bb2a3bf2c48b055ec65db492d627dcca
Eric Polanco <eric@polanco.sh>
2025-04-19 16:28:48 -0400

    removing unused imports

--- components/info-section.tsx
+++ components/info-section.tsx
@@ -1,7 +1,5 @@
 "use client";
 import React from 'react'
-import { useEffect, useRef } from "react";
-import { animate } from "motion";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
